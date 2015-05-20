e13a4b0af61414a86872d96f5b578a01a32767d1
Sonia <sonia.kozubek@gmail.com>
2015-05-20 16:09:22 +0200

    dodaje kalkulator

---
+++ kalkulator.py
@@ -0,0 +1,16 @@
+def dodawanie(a,b):
+	return a+b
+	
+def get_info():
+	print("To je prosty kalkulatoroslaw")
+	
+get_info()
+
+print('podaj pierwsza liczbe')
+a=int(input())
+print('podaj druga liczbe')
+b=int(input())
+
+print(dodawanie(a,b))
+
+
